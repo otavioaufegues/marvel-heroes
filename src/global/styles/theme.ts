ac584eab52b7d0bd6430e8a568ce78a769574862
Otavio Augusto <otavioaufegues@gmail.com>
2023-04-22 15:47:29 -0300

    feat: theme provider

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    primary: '#D42026',
+    primary_light: 'rgba(212, 32, 38,0.1)',
+    secundary: 'rgba(212, 32, 38,0.35)',
+
+    text: '#4E4E4E',
+    text_light: '#A5A5A5',
+    background: '#fff',
+  },
+
+  fonts: {
+    light: 'Roboto_300Light',
+    regular: 'Roboto_400Regular',
+    black: 'Roboto_900Black',
+  },
+};
