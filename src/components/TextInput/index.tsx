9e9e3d18d1be47a3bfbbcbf442bb9bd7fccaa87e
Otavio Augusto <otavioaufegues@gmail.com>
2023-04-23 17:21:19 -0300

    feat : input text component

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -3,14 +3,22 @@ import React from 'react';
 import { Container, SearchInput, SearchLabel } from './styles';
 
 interface Props {
+  value: string;
   label: string;
+  onChangeText?: (args: any) => any | void;
+  onEndEditing?: (args: any) => any | void;
 }
 
-export function TextInput({ label }: Props) {
+export function TextInput({ label, value, onChangeText, onEndEditing }: Props) {
   return (
     <Container>
       <SearchLabel>{label}</SearchLabel>
-      <SearchInput />
+      <SearchInput
+        value={value}
+        onChangeText={onChangeText}
+        onEndEditing={onEndEditing}
+        returnKeyType="done"
+      />
     </Container>
   );
 }
